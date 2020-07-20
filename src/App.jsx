379eb4ed865d39344558d94539db72456bd1274f
floriansr <sueurfloriansf@gmail.com>
2020-07-20 18:29:04 +0200

    [add] StyletronProvider engine

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,28 @@
 import React from 'react';
+import { Provider as StyletronProvider, DebugEngine } from 'styletron-react';
+import { Client as Styletron } from 'styletron-engine-atomic';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import Products from 'components/Products';
+
+const debug =
+  process.env.NODE_ENV === 'production' ? void 0 : new DebugEngine();
+
+const engine = new Styletron();
 
 const App = () => {
-  return <></>;
+  return (
+    <>
+      <StyletronProvider value={engine} debug={debug} debugAfterHydration>
+        <Router>
+          <Switch>
+            <Products exact path="/products" />
+            <Route path="*" status={404} />
+          </Switch>
+        </Router>
+      </StyletronProvider>
+    </>
+  );
 };
 
 export default App;
---
+++ src/components/Products.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Products = () => {
+  console.log('products babe');
+  return (
+    <>
+      <h3>Products</h3>
+    </>
+  );
+};
+
+export default Products;
