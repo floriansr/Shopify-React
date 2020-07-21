f97c14896537709a288c7c92b24568fdb01d618e
floriansr <sueurfloriansf@gmail.com>
2020-07-21 20:18:21 +0200

    [add] styling grid

--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import store from 'redux/store';
+import 'assets/scss/styles.scss';
 
 import App from 'App';
 import * as serviceWorker from './serviceWorker';
