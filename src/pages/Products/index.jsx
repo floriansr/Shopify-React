dcf0acdeeff6a87ea879896114b2ad363270cb0a
floriansr <sueurfloriansf@gmail.com>
2020-07-21 16:11:57 +0200

    [add] display products

--- src/pages/Products/index.jsx
+++ src/pages/Products/index.jsx
@@ -1,27 +1,51 @@
 import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { Link } from 'react-router-dom';
 import ShopifyProvider from 'services/ShopifySDK_Manager';
+
+import { Container, Text, Div, Row, Col } from 'atomize';
 import { setCheckout, setProducts } from '../../redux';
 
 const Products = () => {
   const dispatch = useDispatch();
+  const { products } = useSelector((state) => state.shop);
 
   useEffect(() => {
     const checkout = async () => {
       const res = await ShopifyProvider.createCheckout();
       dispatch(setCheckout(res));
     };
-    const products = async () => {
+    const allProducts = async () => {
       const res = await ShopifyProvider.fetchAllProducts();
       dispatch(setProducts(res));
     };
     checkout();
-    products();
+    allProducts();
   });
 
   return (
     <>
-      <p>products page</p>
+      <Container>
+        <Row>
+          {products.map((product) => (
+            <Col key={product.id} size="3">
+              <Link to="/product/productid">
+                <Div p="2rem">
+                  <Div
+                    h="20rem"
+                    bgImg={product.images[0].src}
+                    bgSize="cover"
+                    bgPos="center center"
+                  >
+                    <Text>{product.title}</Text>
+                    <Text>{product.variants[0].price}</Text>
+                  </Div>
+                </Div>
+              </Link>
+            </Col>
+          ))}
+        </Row>
+      </Container>
     </>
   );
 };
