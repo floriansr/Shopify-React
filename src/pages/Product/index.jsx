bdb9431adaf4eece1ede15e3605d0f77f77742fd
floriansr <sueurfloriansf@gmail.com>
2020-07-21 17:23:08 +0200

    [add] fetch singular product and display infos

--- src/pages/Product/index.jsx
+++ src/pages/Product/index.jsx
@@ -1,14 +1,48 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import ShopifyProvider from 'services/ShopifySDK_Manager';
+import { Container, Text, Div, Row, Col } from 'atomize';
+
+import { setProduct } from '../../redux';
 
 const Product = () => {
   const { id } = useParams();
+  const { product } = useSelector((state) => state.shop);
+  const dispatch = useDispatch();
 
-  return (
-    <>
-      <p>{id}</p>
-    </>
-  );
+  useEffect(() => {
+    const fetchProduct = async () => {
+      const res = await ShopifyProvider.fetchAllProductWithId(id);
+      dispatch(setProduct(res));
+    };
+    fetchProduct();
+  }, [id, dispatch]);
+
+  const renderedItems = () => {
+    return (
+      <>
+        <Container>
+          <Row>
+            <Col>
+              <Div
+                bgImg={product.images[0].src}
+                bgSize="cover"
+                bgPos="center center"
+                h="40rem"
+              />
+            </Col>
+            <Col>
+              <Text>{product.title}</Text>
+              <Text>{product.variants[0].price}</Text>
+            </Col>
+          </Row>
+        </Container>
+      </>
+    );
+  };
+
+  return <>{!product.title ? '' : renderedItems()}</>;
 };
 
 export default Product;
