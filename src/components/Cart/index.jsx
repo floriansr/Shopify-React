c13bf4f085f0aa1d94eb3e5d5585c653bced9000
floriansr <sueurfloriansf@gmail.com>
2020-07-22 19:09:46 +0200

    [add] cart infos

--- src/components/Cart/index.jsx
+++ src/components/Cart/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { SideDrawer, Div, Row, Col } from 'atomize';
+import { SideDrawer, Div, Row, Col, Text } from 'atomize';
 import { removeCart } from '../../redux';
 
 const Cart = () => {
@@ -21,11 +21,20 @@ const Cart = () => {
                 <Col>
                   <Div
                     bgImg={item.variant.image.src}
-                    bgSize="cover"
+                    bgSize="contain"
+                    bgRepeat="no-repeat"
                     bgPos="center"
                     h="5rem"
                     w="4rem"
                   />
+                </Col>
+                <Col>
+                  <Text>{item.title}</Text>
+                  <Text>{item.variant.title}</Text>
+                  <Text>{item.quantity}</Text>
+                </Col>
+                <Col>
+                  <Text>{item.variant.price}</Text>
                 </Col>
               </Row>
             ))}
