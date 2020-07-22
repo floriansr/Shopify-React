d9a6d3c371f6149ebe80675b3bc56b81aeb9c60d
floriansr <sueurfloriansf@gmail.com>
2020-07-22 17:58:05 +0200

    [add] antdesign css

--- src/index.jsx
+++ src/index.jsx
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux';
 import store from 'redux/store';
 import 'assets/scss/styles.scss';
 import 'assets/scss/boomerang/boomerang.scss';
+import 'antd/dist/antd.css';
 
 import App from 'App';
 import * as serviceWorker from './serviceWorker';
