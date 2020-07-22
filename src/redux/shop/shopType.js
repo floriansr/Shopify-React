30ea3e11952e3425278e74778adf8ce53a5e7fbb
floriansr <sueurfloriansf@gmail.com>
2020-07-22 18:00:40 +0200

    [add] new redux action setCart/removeCart

--- src/redux/shop/shopType.js
+++ src/redux/shop/shopType.js
@@ -1,3 +1,5 @@
 export const SET_PRODUCTS = 'SET_PRODUCTS';
 export const SET_PRODUCT = 'SET_PRODUCT';
 export const SET_CHECKOUT = 'SET_CHECKOUT';
+export const SET_CART = 'SET_CART';
+export const REMOVE_CART = 'REMOVE_CART';
