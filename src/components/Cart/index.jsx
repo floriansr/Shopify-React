336d10d45371bd36a9688d8aa0ef4eb905559d71
floriansr <sueurfloriansf@gmail.com>
2020-07-22 18:46:32 +0200

    [add] small object image into SideDrawer

--- src/components/Cart/index.jsx
+++ src/components/Cart/index.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { SideDrawer } from 'atomize';
+import { SideDrawer, Div, Row, Col } from 'atomize';
 import { removeCart } from '../../redux';
 
 const Cart = () => {
   const dispatch = useDispatch();
-  const { isCartOpen } = useSelector((state) => state.shop);
+  const { isCartOpen, checkout } = useSelector((state) => state.shop);
 
   const close = () => {
     dispatch(removeCart());
@@ -14,7 +14,22 @@ const Cart = () => {
   return (
     <>
       <SideDrawer isOpen={isCartOpen} onClose={close}>
-        test
+        <Div d="flex" flexDir="column" m={{ b: '4rem' }}>
+          {checkout.lineItems &&
+            checkout.lineItems.map((item) => (
+              <Row key={item.id}>
+                <Col>
+                  <Div
+                    bgImg={item.variant.image.src}
+                    bgSize="cover"
+                    bgPos="center"
+                    h="5rem"
+                    w="4rem"
+                  />
+                </Col>
+              </Row>
+            ))}
+        </Div>
       </SideDrawer>
     </>
   );
