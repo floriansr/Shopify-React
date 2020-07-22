265a9ba7750621d7d72e42899d55c830e7dc141d
floriansr <46346593+floriansr@users.noreply.github.com>
2020-07-22 12:42:34 +0200

    Stylingv2 with boomerang (#8)
    * [add] boomerang theme scss style
    * [add] row-wrapper for objects
    * [change] styling buttons filter

--- src/pages/Products/index.jsx
+++ src/pages/Products/index.jsx
@@ -3,7 +3,7 @@ import { useSelector, useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 import ShopifyProvider from 'services/ShopifySDK_Manager';
 
-import { Container, Text, Div, Row, Button } from 'atomize';
+import { Container, Div, Row } from 'atomize';
 import { setCheckout, setProducts } from '../../redux';
 
 const Products = () => {
@@ -32,53 +32,107 @@ const Products = () => {
     <>
       <Container>
         <h2>What items are you looking for ?</h2>
-        <Container className="mb-3">
-          <Row>
-            <Button type="button" onClick={(e) => set(e)}>
-              Chairs
-            </Button>
-            <Button type="button" onClick={(e) => set(e)}>
-              Sofas
-            </Button>
-            <Button type="button" onClick={(e) => set(e)}>
-              Lamp
-            </Button>
-            <Button type="button" onClick={(e) => set(e)}>
-              Sofas
-            </Button>
-          </Row>
-        </Container>
 
-        <Row>
-          {products
-            .filter((x) => x.productType === choice)
-            .map((p) => (
-              <Div className="card col-lg-6" key={p.id}>
-                <Div className="card-image">
-                  <Link to={`/product/${p.id}`}>
-                    <Div h="20rem" bgImg={p.images[0].src} bgSize="cover" />
-                  </Link>
-                </Div>
-                <Div className="card-caption-overlay card-caption-overlay--2">
-                  <Div className="card-caption">
-                    <Div className="text-center">
-                      <Link
-                        to={`/product/${p.id}`}
-                        className="heading heading-6 strong-500 mb-0"
-                      >
-                        <Text>{p.title}</Text>
-                      </Link>
+        <section className="slice bg-minimalist">
+          <Container>
+            <div className="row align-items-center mb-5">
+              <div className="col-md-12">
+                <div className="text-center">
+                  <h3 className="heading heading-3 strong-500 text-capitalize">
+                    Featured products
+                  </h3>
+                </div>
+              </div>
+            </div>
+            <Div
+              d="flex"
+              justify="center"
+              className="link-menu link-menu--style-3 mb-5 align-items-center"
+            >
+              <button
+                type="button"
+                onClick={(e) => set(e)}
+                className={choice === 'Chairs' ? 'active' : ''}
+              >
+                Chairs
+              </button>
+              <button
+                type="button"
+                onClick={(e) => set(e)}
+                className={choice === 'Sofas' ? 'active' : ''}
+              >
+                Sofas
+              </button>
+              <button
+                type="button"
+                onClick={(e) => set(e)}
+                className={choice === 'Tables' ? 'active' : ''}
+              >
+                Tables
+              </button>
+              <button
+                type="button"
+                onClick={(e) => set(e)}
+                className={choice === 'Lamps' ? 'active' : ''}
+              >
+                Lamps
+              </button>
+              <button
+                type="button"
+                onClick={(e) => set(e)}
+                className={choice === 'Kit' ? 'active' : ''}
+              >
+                Kit
+              </button>
+              <button
+                type="button"
+                onClick={(e) => set(e)}
+                className={choice === 'Decoration' ? 'active' : ''}
+              >
+                Decoration
+              </button>
+            </Div>
+            <div className="row-wrapper">
+              <div className="cols-xs-space cols-md-space">
+                <Row>
+                  {products
+                    .filter((x) => x.productType === choice)
+                    .map((pa) => (
+                      <div className="col-lg-3 col-md-6" key={pa.id}>
+                        <div className="block product no-border z-depth-2--hover mb-5">
+                          <div className="block-image">
+                            <Link to={`/product/${pa.id}`}>
+                              <Div
+                                h="10rem"
+                                m="3rem"
+                                bgImg={pa.images[0].src}
+                                bgSize="contain"
+                                bgRepeat="no-repeat"
+                                bgPos="center"
+                              />
+                            </Link>
+                          </div>
 
-                      <span className="clearfix" />
-                      <span className="text-sm">
-                        <Text>{p.variants[0].price}</Text>
-                      </span>
-                    </Div>
-                  </Div>
-                </Div>
-              </Div>
-            ))}
-        </Row>
+                          <div className="block-body px-0 text-center">
+                            <h3 className="heading heading-6 strong-500 text-capitalize">
+                              <Link to={`/product/${pa.id}`}>{pa.title}</Link>
+                            </h3>
+                            <div className="price-wrapper">
+                              <span className="price heading-6 c-gray-light strong-400">
+                                <span className="price-value">
+                                  {pa.variants[0].price} $
+                                </span>
+                              </span>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    ))}
+                </Row>
+              </div>
+            </div>
+          </Container>
+        </section>
       </Container>
     </>
   );
